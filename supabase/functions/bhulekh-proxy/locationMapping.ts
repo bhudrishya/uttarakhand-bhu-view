4a1247852e2b61fa7635435a5bac02295689c47c
Sid <sid@Sids-MacBook-Air.local>
2025-11-01 23:12:11 +0100

    sorting the distritct and village

---
+++ supabase/functions/bhulekh-proxy/locationMapping.ts
@@ -0,0 +1,10 @@
+// locationMapping.ts
+
+export const locationMapping: Record<string, { lat: number; lon: number }> = {
+    "00306-045452": { lat: 30.201209565046252, lon: 78.81493082531549 }, // Rishikesh
+    "00307-045453": { lat: 30.325564, lon: 78.043681 }, // Dehradun
+    "00201-032120": { lat: 29.946, lon: 78.160 }, // Haridwar
+    "00402-056321": { lat: 29.85, lon: 79.90 }, // Almora
+    // ➕ Add more tehsil-village combinations here
+  };
+  
